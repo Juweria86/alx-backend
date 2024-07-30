35a75dcad98d292998fd93dd9034ab3168e0a98d
Juweria86 <jujumelmi143@gmail.com>
2024-07-30 15:14:47 +0300

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-""""basic flask module"""
+"""Basic flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    """"return html template"""
-    return render_template("index.html")
+    """returns html template"""
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
